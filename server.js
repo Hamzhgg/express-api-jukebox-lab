1d008945f3805562d2af53c303b778449c5439da
Hamzhgg <gharibehhamzh@gmail.com>
2024-11-10 15:04:19 +0300

    all

---
+++ server.js
@@ -0,0 +1,21 @@
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const methodOverride = require('method-override');
+const cors = require('cors');
+// 
+require('./config/database');
+const app = express();
+const port = process.env.PORT ? process.env.PORT : "3000";
+const trackRouter = require('./controllers/track.js');
+// 
+app.use(morgan('dev'));
+app.use(methodOverride('_method'));
+app.use(express.json());
+app.use(cors({origin: process.env.CORS_ORIGIN}));
+// Routes
+app.use('/tracks', trackRouter);
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
